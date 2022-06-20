0b74f2dc2bca46c70b5634a5e60b57f4a42e47a8
atvfool <atvfool@gmail.com>
2022-06-20 18:03:31 -0400

    Updates constants template with more options

--- constants_template.py
+++ constants_template.py
@@ -1,3 +1,6 @@
 SERVER_SHARE="Server Location"
 FILES = ["file1", "file2"]
 FILE_REPLACE = "string to remove from file name"
+PRINT_FILES = False
+DESTINATION = './'
+PLEX_ADDR = 'http://[IPADDR]:[PORT]' # reserved for future use
